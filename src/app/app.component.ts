0dc5ddee99bc20e3e2f9b425fea2be57fca0198b
Donatas <donkuprys@gmail.com>
2021-10-18 20:20:57 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   public vardas: string;
-  public garbesTaskai: number;
+  public garbesTaskai: number = 0;
 
-  public constructor(){
-    this.vardas = 'Gitanas';
+  public constructor() {
+    this.vardas = "Gitanas";
+  }
+
+  public pridetiTasku(): void {
+    this.garbesTaskai++;
+  }
+
+  public atimtiTasku(): void {
+    this.garbesTaskai--;
   }
 }
